af9e9b2fb8e27f4f5f76bb357fbb8860daea8257
Uzeyir <uzeyir.bagirov@gmail.com>
2021-08-21 10:26:52 -0400

    order cart finished

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -18,11 +18,31 @@ const cartReducer = (state, action) => {
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
     } else {
-      const updatedItems = state.items.concat(action.item);
+         updatedItems = state.items.concat(action.item);
     }
 
    
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+  if(action.type === 'REMOVE'){
+     const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if(existingItem.amount === 1){
+      updatedItems = state.items.filter( item=> item.id !== action.id);
+    } else{
+
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
